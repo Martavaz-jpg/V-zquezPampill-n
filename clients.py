05f1e1785d3123c2175575ef4290a7ad465bf2b4
a19martavp <ateneavaz@gmail.com>
2020-10-28 11:29:01 +0100

    Verson2

---
+++ clients.py
@@ -0,0 +1,42 @@
+import var
+
+class Clientes():
+
+    def validarDni(dni):
+        '''Codigo que controla si el dni es correcto'''
+        try:
+            tabla = 'TRWAGMYFPDXBNJZSQVHLCKE'
+            dig_ext = 'XYZ'
+            reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
+            numeros = '1234567890'
+            dni = dni.upper()
+            if len(dni) == 9:
+                dig_control = dni[8]
+                dni = dni[:8]
+                if dni[0] in dig_ext:
+                    dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+                return len(dni == len([n for n in dni if n in numeros])) and tabla[int(dni) % 23] == dig_control
+
+        except:
+            print('Error en el módulo de validación del DNI')
+            return None
+
+
+
+
+    def validoDni():
+        try:
+            '''muestra mensaje de dni valido'''
+            dni=var.ui.leDni.text()
+            if Clientes.validarDni(dni):
+                var.ui.lbValidar.setStyleSheet('QLabel{color:green;}')
+                var.ui.lbValidar.setText('V')
+                var.ui.leDni.setText(dni.upper())
+            else:
+                var.ui.lbValidar.setStyleSheet('QLabel{color:red;}')
+                var.ui.lbValidar.setText('X')
+                var.ui.leDni.setText(dni.upper())
+
+        except:
+            print('ERROR modulo validar dni')
+            return None
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from untitled import *
-import sys,var,events
+from ventana import *
+import sys,var,events,clients
 
-class Main(QTwidgets.QMainWindows):
+class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
-        var.ui= Ui_MainWindows()
-        var.ui.setupUi()
-'''Conexion de eventos con objetos'''
-        var.ui.bnCerrar.cliced.connect(events.eventos.Salir)
-        var.ui.actionSalir.triggered.connect(events.Eventos.Salir)
+        var.ui= Ui_venPrincipal()
+        var.ui.setupUi(self)
+        '''
+        Conexion de eventos con objetos
+        '''
+        var.ui.bnCerrar.clicked.connect(events.Eventos.Salir)
+        var.ui.leDni.editingFinished.connect(clients.Clientes.validoDni)
 
-if __name__='__main__':
-    app=QtWidgets.QApplication([])
-    window=Main()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window =  Main()
     window.show()
     sys.exit(app.exec())
